bdccc96b3028c99595a9fd5dceab2cd2782857d0
pstech <pstech@DESKTOP-AE8VB6G.localdomain>
2021-02-12 17:50:02 +0100

    gjort färdigt väldigt enkelt login

--- routes/login.js
+++ routes/login.js
@@ -5,7 +5,56 @@ const { query } = require('../models/db');
 
 /* GET login form */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  // res.send('respond with a resource');
+  res.render('login', {title: 'Login'});
 });
+
+/* GET skapa en hash */
+router.get('/kryptan/:pwd', function(req, res, next) {
+
+  const myPlaintextPassword = req.params.pwd;
+
+  bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
+    res.json({
+      pwd: hash
+    });
+  });
+
+
+});
+
+/* POST login */
+router.post('/', async function(req, res, next) {
+
+  const username = req.body.username;
+  const password = req.body.password;
+
+  if (username && password) {
+    try {
+      const sql = 'SELECT password FROM users WHERE name = ?';
+      const result = await query(sql, username);
+
+      if(result.length > 0) {
+        bcrypt.compare(password, result[0].password, function(err, result) {
+          if (result == true) {
+            req.session.loggedin = true;
+            req.session.username = username;
+            res.redirect('/secret');
+          } else {
+            res.render('login',{ error: 'Wrong username or password!'});
+          }
+        });
+      } else {
+        res.render('login',{ error: 'Wrong username or password!'});
+      }
+    } catch (e) {
+      next(e);
+      console.error(e);
+    }
+  } else {
+    res.render('login',{ error: 'Wrong username or password!'});
+  }
+});
+
 
 module.exports = router;
