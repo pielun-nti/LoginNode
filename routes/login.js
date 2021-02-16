4c0303bfea41590c905a65f203a83d09b0ffbf94
root <root@DESKTOP-AE8VB6G.localdomain>
2021-02-16 18:35:18 +0100

    refractored index, login routes

--- routes/login.js
+++ routes/login.js
@@ -1,57 +1,41 @@
 const express = require('express');
 const router = express.Router();
 const bcrypt = require('bcrypt');
+const authcontroller = require('../controllers/AuthController');
 const { query } = require('../models/db');
+const { body, validationResult } = require('express-validator');
 
+/* GET login form */
 router.get('/', function(req, res, next) {
-  res.render('login', {title: 'Login'});
+  res.render('login');
 });
 
+/* GET skapa en hash som vi kan spara i db */
 router.get('/kryptan/:pwd', function(req, res, next) {
 
   const myPlaintextPassword = req.params.pwd;
 
   bcrypt.hash(myPlaintextPassword, 10, function(err, hash) {
+    // Store hash in your password DB.
     res.json({
       pwd: hash
     });
   });
-
-
 });
 
 /* POST login */
-router.post('/', async function(req, res, next) {
+router.post('/', 
+  body('username').notEmpty().trim(),
+  body('password').notEmpty(),
+  authcontroller.store);
 
-  const username = req.body.username;
-  const password = req.body.password;
-
-  if (username && password) {
-    try {
-      const sql = 'SELECT password FROM users WHERE name = ?';
-      const result = await query(sql, username);
-
-      if(result.length > 0) {
-        bcrypt.compare(password, result[0].password, function(err, result) {
-          if (result == true) {
-            req.session.loggedin = true;
-            req.session.username = username;
-            res.redirect('/secret');
-          } else {
-            res.render('login',{ error: 'Wrong username or password!'});
-          }
-        });
-      } else {
-        res.render('login',{ error: 'Wrong username or password!'});
-      }
-    } catch (e) {
-      next(e);
-      console.error(e);
-    }
-  } else {
-    res.render('login',{ error: 'Wrong username or password!'});
-  }
-});
-
+router.delete('/', authcontroller.destroy);
 
 module.exports = router;
+
+/*
+"value": "",
+"msg": "Invalid value",
+"param": "username",
+"location": "body"
+*/
