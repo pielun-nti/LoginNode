aafe5f3e47bbb7aff6244c15e4b61bb3b87453f0
pstech <pierrelundstrom21@outlook.com>
2021-02-26 19:52:40 +0100

    refractor(hometest): changed hometest to check authentication after logging in and check logout

--- test/hometest.js
+++ test/hometest.js
@@ -1,26 +1,37 @@
 const expect = require('chai').expect;
 const app = require('../app');
-const request = require('supertest')(app);
+// const request = require('supertest');
+const session = require('supertest-session')(app);
 
-// testet börjar här
-describe('index route', () => {
-  describe('GET /', () => {
-    // vad förväntar vi oss ska ske, it should return...
-    it('should return OK status', () => {
-      // utför requesten, kontrollera att den svarar 200 och avsluta sedan testet
-      request.get('/')
-        .expect(200)
-        .end((err, res) => {
-          if (err) throw err;
-        });
-    });
-  });
-  it('should greet the user', (done) => {
-    request.get('/')
-      .end((err, res) => {
-        if (err) throw err;
-        expect(res.text).to.contain('Welcome')
+// const testSession = null;
+
+describe('/home', () => {
+  let authenticatedSession = null;
+
+  beforeEach((done) => {
+    session.post('/login')
+      .type('form')
+      .send({
+        username: process.env.TEST_USER,
+        password: process.env.TEST_PASSWORD
+      })
+      .expect(302)
+      .end(function (err) {
+        if (err) return done(err);
+        authenticatedSession = session;
         return done();
       });
+  });
+
+  it('should get a restricted page', function (done) {
+    authenticatedSession.get('/home')
+      .expect(200)
+      .end(done)
+  });
+
+  it('should log out a user', function (done) {
+    authenticatedSession.post('/logout')
+      .expect(302)
+      .end(done)
   });
 }); 
